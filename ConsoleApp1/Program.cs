f3faa58163a6bfa57347947edf2b8245e249dfca
Ahmtdkyuteoi7 <ahmadconhikmatov@gmail.com>
2025-04-29 14:57:57 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,10 @@
 ﻿int password = 12345;
+// верный пароль
 bool a = true;
 while (a == true)
 {
     int password1 = Convert.ToInt32(Console.ReadLine());
+    // запрашивает пароль
     if(password == password1)
     {
         Console.WriteLine("Пароль принят");
