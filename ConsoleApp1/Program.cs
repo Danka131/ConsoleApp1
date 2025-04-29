2917e2b892e5615ea53bcc5ade96fff5e6942e89
Mijgona Mahmudova <mishel167j@gmail.com>
2025-04-29 14:32:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿int password = 12345;
+bool a = true;
+while (a == true)
+{
+    int password1 = Convert.ToInt32(Console.ReadLine());
+    if(password == password1)
+    {
+        Console.WriteLine("Пароль принят");
+        a = false;
+    }
+    else
+    {
+        Console.WriteLine("Повторите попытку");
+    }
+}
