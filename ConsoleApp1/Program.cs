dfcc4875f1896b3e0382d192be8d13799c0ff6df
timofeika226 <tkazakov38@gmail.com>
2025-04-29 15:22:32 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,12 +6,12 @@ while (a == true)
     int password1 = Convert.ToInt32(Console.ReadLine());
     // запрашивает пароль
     if(password == password1)
-    {
+    {"Если пороль верный то"
         Console.WriteLine("Пароль принят");
-        a = false;
+        a = false;"цикл закрывается"
     }
     else
-    {
+    {"если код не верный то выводится строка (повторите попытку)"
         Console.WriteLine("Повторите попытку");
     }
 }
